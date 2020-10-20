6de6a4cc107b17dbbf220817809d58245de55cd4
Nathan Lewis <112nathan112@gmail.com>
2020-10-20 22:25:44 +0100

    Commited files.

---
+++ firstFrameWork/firstFrameWork.h
@@ -0,0 +1,18 @@
+//
+//  firstFrameWork.h
+//  firstFrameWork
+//
+//  Created by Nathan Lewis Kuruvilla  on 17/10/2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for firstFrameWork.
+FOUNDATION_EXPORT double firstFrameWorkVersionNumber;
+
+//! Project version string for firstFrameWork.
+FOUNDATION_EXPORT const unsigned char firstFrameWorkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <firstFrameWork/PublicHeader.h>
+
+
---
+++ firstFrameWork/plusTen.swift
@@ -0,0 +1,25 @@
+//
+//  plusTen.swift
+//  firstFrameWork
+//
+//  Created by Nathan Lewis Kuruvilla on 19/10/2020.
+//
+
+import Foundation
+
+
+public class plusTen {
+    
+    // set the initializer to public, otherwise you cannot invoke class
+    public init() {
+
+    }
+    
+    public func add(nbr: Int) -> Int {
+        
+        let nbr2 = nbr+10
+        return nbr2
+    }
+    
+    
+}
